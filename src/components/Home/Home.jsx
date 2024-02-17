f88a62988324b2119ea29e81913f1ab683201770
recheitor <arc19881@gmail.com>
2024-02-17 11:29:43 +0100

    title

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,15 +8,56 @@ function Home() {
       logo.classList.add('animate-logo');
     }, 1000 )
     
-  }, []); // Empty dependency array to ensure the effect runs only once on mount
+  }, []); 
 
   return (
     <>
-      <div className='initial-logo'>
-        <img src="src/assets/images/TezixLogo-XL.webp" alt="YEEE" />
-      </div>
+    <div className='initial-logo'>
+        <svg id="butterfly1" width="333" height="281" viewBox="0 0 333 281" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g >
+<path id="left" d="M12.727 90.0064C-8.27295 53.0064 0.992666 17.3251 24.227 5.00637C42.3946 -4.31245 77 3.6975 95.2271 18.0064C99.7432 21.5517 120.994 40.0487 134.227 58.0064C189.727 129.506 183.227 207.006 128.727 258.506C99.2271 283.006 75.5 284.5 50 273C91 247.5 114.841 205.006 84.2271 165.006C65.9865 141.174 54.6869 129.818 33.227 112.006C23.1172 103.17 18.7768 98.3702 12.727 90.0064Z" stroke="black" fill="url(#gradient1)"/>
+<path id="leftbottom" d="M84.5 165.5C115.114 205.5 91 247.5 50 273C21.9071 254.068 17.7557 233.5 27.7557 208C35.7557 191 53.5697 175.761 84.5 165.5Z" stroke="black" fill="url(#gradient2)"/>
+<path id="right" d="M320.123 90.0064C341.122 53.0064 331.857 17.3251 308.623 5.00637C290.455 -4.31245 255.849 3.6975 237.622 18.0064C233.106 21.5517 211.856 40.0487 198.622 58.0064C143.122 129.506 149.622 207.006 204.122 258.506C233.622 283.006 257.349 284.5 282.849 273C241.849 247.5 218.008 205.006 248.622 165.006C266.863 141.174 278.163 129.818 299.622 112.006C309.732 103.17 314.073 98.3702 320.123 90.0064Z" stroke="black" fill="url(#gradient3)"/>
+<path id="rightbottom" d="M248.349 165.5C217.735 205.5 241.849 247.5 282.849 273C310.942 254.068 315.094 233.5 305.094 208C297.094 191 279.28 175.761 248.349 165.5Z" stroke="black" fill="url(#gradient4)"/>
+<path id="center" d="M166.5 118C175.249 152.795 173.548 170.464 166.5 200.86C157.815 170.071 157.671 151.953 166.5 118Z" stroke="black" fill='#C97791'/>
+</g>
+<defs>
+        <linearGradient id="gradient1" x1="179" y1="149.5" x2="-3.49288e-06" y2="152.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FEBAAF"/>
+            <stop offset="1" stop-color="#FFC2A2"/>
+        </linearGradient>
+        <linearGradient id="gradient2" x1="104" y1="218" x2="10.5" y2="218" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#D88B97"/>
+            <stop offset="1" stop-color="#E49699"/>
+        </linearGradient>
+        <linearGradient id="gradient3" x1="151.849" y1="149.5" x2="330.849" y2="152.5" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#FEB9B1"/>
+            <stop offset="1" stop-color="#FEB1BD"/>
+        </linearGradient>
+        <linearGradient id="gradient4" x1="229" y1="218" x2="315" y2="218" gradientUnits="userSpaceOnUse">
+            <stop stop-color="#B66388"/>
+            <stop offset="1" stop-color="#AC5585"/>
+        </linearGradient>
+      
+    </defs>
+</svg>
+
+<svg id="letters" width="1171" height="284" viewBox="0 0 1171 284" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path  d="M0.525568 62.4091V0.363628H247.045V62.4091H161.733V284H85.8381V62.4091H0.525568ZM276.025 284V0.363628H480.443V62.4091H353.028V111.159H469.917V173.205H353.028V221.955H479.889V284H276.025ZM519.741 284V241.344L653.803 62.4091H520.295V0.363628H751.857V43.0199L617.795 221.955H751.303V284H519.741ZM866.427 0.363628V284H789.424V0.363628H866.427ZM985.463 0.363628L1032 82.9062H1034.21L1081.3 0.363628H1167.17L1082.41 142.182L1170.49 284H1082.41L1034.21 199.795H1032L983.801 284H896.272L983.247 142.182L899.042 0.363628H985.463Z" fill="url(#paint0_linear_131_139)"/>
+<defs>
+<linearGradient id="paint0_linear_131_139" x1="1183" y1="142" x2="-10" y2="142" gradientUnits="userSpaceOnUse">
+<stop stop-color="#AD5686"/>
+<stop offset="1" stop-color="#E5979A"/>
+</linearGradient>
+</defs>
+</svg>
+
+</div>
+
+
     </>
   );
 }
 
 export default Home;
+
