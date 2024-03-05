da3517d3ba99c4ad0738c232e33239ee267d0bf2
recheitor <arc19881@gmail.com>
2024-03-05 22:11:59 +0100

    cards

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,55 +1,11 @@
 import './Hero.css'
 function Hero() {
 
-  const animatedTextTop = 'building'
-  const animatedTextBottom = 'ideas'
+
 
   return (
-    <>
-    {/* <div className='hero'>
-      <div className='title'>
-        <div className="hide-text">
-          <div className="top hero-text">
-          <div className="text-0">
-            <h1 className='outfit-extralight'>We help you</h1>
-            </div>
-            <div className="text-1">
-            <h1 className='outfit-extralight'>We help you <span className='outfit-regular'>building</span></h1>
-            </div>
-            <div className="text-2">
-            <h1 className='outfit-extralight'>We help you <span className='outfit-regular'>enhacing</span></h1>
-            </div>
-            <div className="text-3">
-            <h1 className='outfit-extralight'>We help you <span className='outfit-regular'>accomplish</span></h1>
-            </div>
-            <div className="text-4">
-            <h1 className='outfit-extralight'>We help you <span className='outfit-regular'>building</span></h1>
-            </div>
-          </div>
-        </div>
-        <div className="hide-text">
-          <div className="bottom hero-text">
-          <div className="text-0">
-            <h1 className='outfit-extralight'>your</h1>
-            </div>
-            <div className="text-1">
-            <h1 className='outfit-extralight'>your <span className='outfit-regular'>ideas</span></h1>
-            </div>
-            <div className="text-2">
-            <h1 className='outfit-extralight'>your <span className='outfit-regular'>business</span></h1>
-            </div>
-            <div className="text-3">
-            <h1 className='outfit-extralight'>your <span className='outfit-regular'>objectives</span></h1>
-            </div>
-            <div className="text-4">
-            <h1 className='outfit-extralight'>your <span className='outfit-regular'>ideas</span></h1>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div> */}
-
-<div className='hero'>
+  <>
+    <div className='hero'>
       <div className='title'>
         <div className="text">
           <div className='static-text-top'>
@@ -73,6 +29,11 @@ function Hero() {
             <h2 className='outfit-regular'>ideas</h2>
           </div>
         </div>
+      </div>
+      <div className="cards">
+        <div className="card"><h2 className="outfit-extralight">Web development</h2></div>
+        <div className="card"><h2 className="outfit-extralight">Design UX/UI</h2></div>
+        <div className="card"><h2 className="outfit-extralight">Marketing</h2></div>
       </div>
     </div>
   </>
