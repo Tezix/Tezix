19cf4ad5def07ef8132ead406548a168d25c59f0
recheitor <arc19881@gmail.com>
2024-03-06 21:36:39 +0100

    font face loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import IntroPage from './pages/IntroPage/IntroPage';
 import  { useState, useEffect } from 'react';
+import FontFaceObserver from 'fontfaceobserver';
 
 function App() {
   const [showNavbarAndBackground, setShowNavbarAndBackground] = useState(false);
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowNavbarAndBackground(true);
-    }, 2000);
+    const font = new FontFaceObserver('Outfit'); // Replace 'Outfit' with the font family name you're using
 
+    font.load().then(() => {
+      const timer = setTimeout(() => {
+        setShowNavbarAndBackground(true);
+      }, 2000);
     return () => clearTimeout(timer);
+
+    });
+
+
+
+   
+
   }, []);
 
   return (
