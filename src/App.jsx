f066f25f6cd308e164547b1f0415127e84b0c220
recheitor <arc19881@gmail.com>
2024-03-06 19:45:45 +0100

    menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import IntroPage from './pages/IntroPage/IntroPage';
+import  { useState, useEffect } from 'react';
 
 function App() {
+  const [showNavbarAndBackground, setShowNavbarAndBackground] = useState(false);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowNavbarAndBackground(true);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <IntroPage />
-    <HomePage/>
+          {showNavbarAndBackground ?
+            <HomePage/>
+            :
+            <IntroPage />
+          }
     </>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,30 +1,21 @@
 import './HomePage.css'
-import  { useState, useEffect } from 'react';
 import Background from './../../components/BackgroundHero/Background';
 import Navbar from './../../components/Navbar/Navbar';
 import Hero from './../../components/Hero/Hero';
 
 function HomePage() {
 
-  const [showNavbarAndBackground, setShowNavbarAndBackground] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowNavbarAndBackground(true);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
+  
     return (
       <>
-          {showNavbarAndBackground && (
         <>
           <Navbar/>
           <Background />
-          
-          <div id='Hero'><Hero /></div>
+          <div id='Hero'>
+            <Hero />
+            </div>
         </>
-      )}
+
       </>
     )
   }
